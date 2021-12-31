efe7515d729040e0eb80e8e637997c2b4b24fc57
mohammed-elattar <mseel3ttar@gmail.com>
2021-12-31 01:57:21 +0100

    setup ingress

--- auth/src/index.ts
+++ auth/src/index.ts
@@ -2,6 +2,10 @@ const express = require('express')
 const app = express()
 const port = 3000
 
+app.get('/api/users/currentuser', (req, res) => {
+  res.send('Hello World!')
+})
+
 app.listen(port, () => {
   console.log(`Example app listening at http://localhost:${port}`)
 })
