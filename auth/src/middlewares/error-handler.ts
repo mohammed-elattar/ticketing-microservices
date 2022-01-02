e88b13bb978d5972acc3de4a5fb65a68636f75b3
mohammed-elattar <mseel3ttar@gmail.com>
2022-01-02 01:59:56 +0100

    unify the error response

--- auth/src/middlewares/error-handler.ts
+++ auth/src/middlewares/error-handler.ts
@@ -1,4 +1,6 @@
 import { Request, Response, NextFunction } from "express";
+import { DatabaseConnectionError } from "../errors/database-connection-error";
+import { RequestValidationError } from "../errors/request-validation-error";
 
 export const errorHandler = (
   err: Error,
@@ -6,9 +8,20 @@ export const errorHandler = (
   res: Response,
   next: NextFunction
 ) => {
-  console.log("Something went wrong", err);
+  if(err instanceof RequestValidationError) {
+      const formattedErrors = err.errors.map( error =>{return {message: error.msg, field: error.param}} );
+      return res.status(400).send({
+        errors: formattedErrors
+      });
+  }
+  
+  if(err instanceof DatabaseConnectionError) {
+    return res.status(500).send({
+        errors: [{message: err.reason}]
+      });
+  }
 
   res.status(400).send({
-    message: "Something went wrong",
+    errors: [{message: err.message}]
   });
 };
