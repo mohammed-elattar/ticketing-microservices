865de1ab9d4af10ab2eeb3bf560173f465efd63f
mohammed-elattar <mseel3ttar@gmail.com>
2022-01-02 22:03:26 +0100

    connecting to mongo db

--- auth/src/index.ts
+++ auth/src/index.ts
@@ -1,5 +1,6 @@
 import express from 'express';
 import 'express-async-errors';
+import mongoose from 'mongoose';
 import { NotFoundError } from "./errors/not-found-error";
 import { errorHandler } from "./middlewares/error-handler";
 import { currentUserRouter } from "./routes/current-user";
@@ -20,6 +21,20 @@ app.all('*', async (req, res) => {
     throw new NotFoundError();
 })
 
+
+const connectDB = async () => {
+  try {
+    await mongoose
+    .connect('mongodb://auth-mongo-srv:27017/auth ');    
+    console.log('connected to mongo db');
+  } catch (error) {
+   console.error(error)   
+  }
+
 app.listen(port, () => {
   console.log(`Example app listening at http://localhost:${port}`)
 })
+
+};
+
+connectDB();
