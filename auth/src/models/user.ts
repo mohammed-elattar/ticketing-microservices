6792fe25c3bc08ad54a5f66d92d40e1b8b94cfa6
mohammed-elattar <mseel3ttar@gmail.com>
2022-01-05 00:13:07 +0100

    formatting json response for user signup

--- auth/src/models/user.ts
+++ auth/src/models/user.ts
@@ -1,4 +1,5 @@
 import mongoose from 'mongoose';
+import { transform } from 'typescript';
 import { password } from '../services/password';
 
 interface UserAttrs {
@@ -20,6 +21,15 @@ const { Schema } = mongoose;
   const userSchema = new Schema({
     email:  {type: String,required: true},
     password:  {type: String,required: true},
+  }, {
+      toJSON: {
+          transform(doc, ret) {
+              ret.id = ret._id;
+              delete ret.__v;
+              delete ret.password;
+              delete ret._id;
+          }
+      }
   });
 
   userSchema.statics.build = (attrs: UserAttrs) => {
