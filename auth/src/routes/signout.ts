22b5861511d02c0ebb72282f7a057deb52a2697c
mohammed-elattar <mseel3ttar@gmail.com>
2022-01-05 23:45:44 +0100

    sign out api

--- auth/src/routes/signout.ts
+++ auth/src/routes/signout.ts
@@ -3,7 +3,9 @@ import express, { Request, Response } from 'express';
 const router = express.Router();
 
 router.post('/api/users/signout', (req: Request, res: Response) => {
-  res.send('Hi there!');
+  req.session = null; 
+
+  return res.send({});
 });
 
 export { router as signoutRouter };
