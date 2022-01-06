eab6221ece948c0bc338a40b07b7b9ede1ff5e10
mohammed-elattar <mseel3ttar@gmail.com>
2022-01-06 23:57:07 +0100

    test sign up different cases

--- auth/src/routes/__test__/signup.test.ts
+++ auth/src/routes/__test__/signup.test.ts
@@ -10,4 +10,36 @@ describe('POST /api/users/signup', function() {
         .send({email: 'test@test.com', password: 'asdfghjkl'})
         .expect(201, done);
     });
+    it('invalid email', function(done) {
+      request(app)
+        .post('/api/users/signup')
+        .set('Accept', 'application/json')
+        .expect('Content-Type', /json/)
+        .send({email: 'testtest.com', password: 'asdfghjkl'})
+        .expect(400, done);
+    });
+    it('invalid password', function(done) {
+      request(app)
+        .post('/api/users/signup')
+        .set('Accept', 'application/json')
+        .expect('Content-Type', /json/)
+        .send({email: 'test@test.com', password: 'as'})
+        .expect(400, done);
+    });
+    it('missing email', function(done) {
+      request(app)
+        .post('/api/users/signup')
+        .set('Accept', 'application/json')
+        .expect('Content-Type', /json/)
+        .send({ password: 'asdfghjkl'})
+        .expect(400, done);
+    });
+    it('missing password', function(done) {
+      request(app)
+        .post('/api/users/signup')
+        .set('Accept', 'application/json')
+        .expect('Content-Type', /json/)
+        .send({email: 'test@test.com'})
+        .expect(400, done);
+    });
   });
