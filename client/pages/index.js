8f97adc0d001c1c75262c522eb13258127cd0fea
mohammed-elattar <mseel3ttar@gmail.com>
2022-01-16 23:43:17 +0100

    distinguish between server and client requests

--- client/pages/index.js
+++ client/pages/index.js
@@ -1,7 +1,15 @@
 import React from 'react';
 
-const index = () => {
+const LandingPage = () => {
   return <div>Mohammed Elattar</div>;
 };
 
-export default index;
+LandingPage.getInitialProps = async () => {
+  if (typeof window === undefined) {
+    // we are on the server and the request should be made to ingress service
+  } else {
+    // we are on the client and the request can be made to the '/api' directly
+  }
+};
+
+export default LandingPage;
