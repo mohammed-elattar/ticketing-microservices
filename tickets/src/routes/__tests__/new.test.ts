29ece419ab322d465dad0e44f219a5196fccaedf
mohammed-elattar <mseel3ttar@gmail.com>
2022-02-05 23:59:17 +0100

    test validation

--- tickets/src/routes/__tests__/new.test.ts
+++ tickets/src/routes/__tests__/new.test.ts
@@ -17,8 +17,28 @@ it('returns a status other than 401 if the user is signed in', async () => {
   expect(response.status).not.toEqual(401);
 });
 
-it('returns an error if an invalid title is provided', async () => {});
+it('returns an error if an invalid title is provided', async () => {
+    await request(app).post('/api/tickets').set('Cookie', global.signIn()).send({
+        'title':'',
+        'price': 10
+    }).expect(400);
+    
 
-it('returns an error if an invalid price is provided', async () => {});
+    await request(app).post('/api/tickets').set('Cookie', global.signIn()).send({
+        'price': 10
+    }).expect(400);
+});
+
+it('returns an error if an invalid price is provided', async () => {
+    await request(app).post('/api/tickets').set('Cookie', global.signIn()).send({
+        'title':'test',
+        'price': -10
+    }).expect(400);
+    
+
+    await request(app).post('/api/tickets').set('Cookie', global.signIn()).send({
+        'title':'test',
+    }).expect(400);
+});
 
 it('creates a ticket with valid inputs', async () => {});
--- tickets/src/routes/new.ts
+++ tickets/src/routes/new.ts
@@ -1,12 +1,18 @@
-import { requireAuth } from '@mseel3ttar/common';
+import { requestValidate, requireAuth } from '@mseel3ttar/common';
 import express, { Request, Response } from 'express';
+import { body } from 'express-validator';
 
 const createTicketRouter = express.Router();
 
 createTicketRouter.post(
     '/api/tickets',
-    requireAuth
+    requireAuth,
+    [
+        body('title').not().isEmpty().withMessage('Title is required'),
+        body('price').isFloat({ gt: 0}).withMessage('Title is required')
+]
     ,
+    requestValidate,
     async (req: Request, res: Response) => {
         res.sendStatus(200);
     });
